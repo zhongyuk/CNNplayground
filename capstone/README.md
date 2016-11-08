9669d46c2bc75f49c09c48224161070cdfac7168
Zhongyu Kuang <dream.kuang@gmail.com>
2016-11-08 10:58:11 -0500

    add readme and report for capstone and reorganize files

---
+++ capstone/README.md
@@ -0,0 +1,27 @@
+## Udacity Machine Learning Nanodegree Capstone Project
+## Image Classification using Convolutional Neural Network
+
+#### Data Source
+[CIFAR-10](http://www.cs.utoronto.ca/~kriz/cifar.html)
+
+#### Programming Language
+**Python 2.7**
+
+#### Software Dependencies
+- TensorFlow
+- Numpy
+- scikit-learn
+- matplotlib
+
+#### Files Explained
+- **In /code**
+
+All the development codes for this project are in /code. For the .py files which end with a numerical number (0~4): the convnet_devp0.py is the earliest development code, convnet_devp4.py is the latest development code. Most of the training data files stored in /train_data are results collected by running convnet_devp2.py with different hyperparameter settings. The prepare_input.py alone file contains the functions for lodaing, preprocessing input data. 
+
+- **In /notebook**
+All the jupyter notebooks end with a numerical number (0~3). The number corresponds to a project development stage (which matches with the numerical number in /code/convnet_devp*.py). Each notebook records from coarse to fine hyperparameter tunings and plots/analyzes some training data stored in /train_data
+
+- **In /train_data**
+Training data is recorded for different CNN models and stored in pickle files. The first numerical number in each pickle data file indicates the corresponding stage of the development code (training_data_stack2.* is collected by running convnet_devp2). The second numerical number correspondes to different hyperparameter settings. The details pertaining to the hyperparameter settings can be found in the corresponding notebook (/notebook/experiment2.ipynb)
+
+
---
+++ capstone/code/convnet_devp4.py
@@ -0,0 +1,324 @@
+import numpy as np
+import tensorflow as tf
+from six.moves import cPickle as pickle
+from sklearn.model_selection import train_test_split
+import time
+from tensorflow.python.training import moving_averages
+from tensorflow.contrib.layers.python.layers import utils
+from prepare_input import *
+
+def compute_accuracy(pred_tensor, label_tensor):
+    '''A func to compute accuracy given TF tensor as inputs'''
+    correct_pred = tf.equal(tf.argmax(pred_tensor,1), tf.argmax(label_tensor,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+    return accuracy
+
+def _initialize_wts_bis(variable_scope, shape, initializer=tf.truncated_normal_initializer(stddev=.01)):
+    with tf.variable_scope(variable_scope) as scope:
+        w = tf.get_variable("wt", shape, initializer=initializer)
+        b = tf.get_variable("bi", shape[-1], initializer=tf.constant_initializer(1.0))
+        variable_summaries(w, variable_scope+'/weights')
+        variable_summaries(b, variable_scope+'/biases')
+        scope.reuse_variables()
+
+
+def _initialize_BN(variable_scope, depth):
+    with tf.variable_scope(variable_scope) as scope:
+        gamma = tf.get_variable("gamma", depth, initializer=tf.constant_initializer(1.0))
+        beta = tf.get_variable("beta", depth, initializer=tf.constant_initializer(0.0))
+        moving_avg = tf.get_variable("moving_avg", depth, initializer=tf.constant_initializer(0.0), trainable=False)
+        moving_var = tf.get_variable("moving_var", depth, initializer=tf.constant_initializer(1.0), trainable=False)
+        variable_summaries(gamma, variable_scope+'/scale_gamma')
+        variable_summaries(beta, variable_scope+'/offset_beta')
+        variable_summaries(moving_avg, variable_scope+'/moving_mean')
+        variable_summaries(moving_var, variable_scope+'/moving_variance')
+        scope.reuse_variables()
+        
+def initialize_variables(cnn_shapes, initializer, batch_norm=False):
+    for item in cnn_shapes:
+        variable_scope, shape = item[0], item[1]
+        _initialize_wts_bis(variable_scope, shape, initializer)
+        if batch_norm:
+            _initialize_BN(variable_scope, shape[-1])
+
+def variable_summaries(var, name):
+    with tf.name_scope('summaries'):
+        avg = tf.reduce_mean(var)
+        tf.scalar_summary('mean/'+name, avg)
+        with tf.name_scope('std'):
+            std = tf.sqrt(tf.reduce_mean(tf.square(var - avg)))
+        tf.scalar_summary('std/'+name, std)
+        tf.scalar_summary('max/'+name, tf.reduce_max(var))
+        tf.scalar_summary('min/'+name, tf.reduce_min(var))
+        tf.histogram_summary(name, var)
+
+
+def pool_layer(x, method='max', kernel=2, stride=2, padding='SAME'):
+    if method=='max':
+        return tf.nn.max_pool(x, [1,kernel,kernel,1], [1,stride,stride,1], padding=padding)
+    elif method=='avg':
+        return tf.nn.avg_pool(x, [1,kernel,kernel,1], [1,stride,stride,1], padding=padding)
+    else:
+        raise ValueError(method+' pooling method does not exist')
+
+def conv_layer(x, variable_scope, stride=1, padding='SAME'):
+    with tf.variable_scope(variable_scope, reuse=True):
+        w = tf.get_variable("wt")
+        b = tf.get_variable("bi")
+        y = tf.nn.conv2d(x, w, [1, stride, stride, 1], padding=padding) + b
+    return y
+
+def fc_layer(x, variable_scope):
+    with tf.variable_scope(variable_scope, reuse=True):
+        w = tf.get_variable("wt")
+        b = tf.get_variable("bi")
+        y = tf.matmul(x, w) + b
+    return y
+
+def batch_norm_layer(x, variable_scope, is_training, epsilon=0.001, decay=.999):
+    with tf.variable_scope(variable_scope, reuse=True):
+        tf.histogram_summary(variable_scope+'/preBN', x)
+        gamma, beta = tf.get_variable("gamma"), tf.get_variable("beta")
+        moving_avg, moving_var = tf.get_variable("moving_avg"), tf.get_variable("moving_var")
+        shape = x.get_shape().as_list()
+        control_inputs = []
+        if is_training:
+            avg, var = tf.nn.moments(x, range(len(shape)-1))
+            update_moving_avg = moving_averages.assign_moving_average(moving_avg, avg, decay)
+            update_moving_var = moving_averages.assign_moving_average(moving_var, var, decay)
+            control_inputs = [update_moving_avg, update_moving_var]
+        else:
+            avg = moving_avg
+            var = moving_var
+        with tf.control_dependencies(control_inputs):
+            y = tf.nn.batch_normalization(x, avg, var, offset=beta, scale=gamma, variance_epsilon=epsilon)
+        tf.histogram_summary(variable_scope+'/postBN', y)
+    return y
+
+
+def cnn_model(data, variable_scopes, is_training, batch_norm, keep_prob):
+    x = data
+    # 3 repetitive conv structures: conv -> batch norm -> ReLu -> max pool
+    for var_scope in variable_scopes[:3]:
+        x = conv_layer(x ,var_scope)
+        if batch_norm:
+            x = batch_norm_layer(x, var_scope, is_training)
+        x = tf.nn.relu(x)
+        x = pool_layer(x)
+    # reshape x to prepare for fully connected layer
+    shape = x.get_shape().as_list()
+    x = tf.reshape(x, [shape[0], -1])
+    # fully connected layer: fc1 -> batch norm -> ReLu -> dropout
+    x = fc_layer(x, variable_scopes[3])
+    if batch_norm:
+        x = batch_norm_layer(x, variable_scopes[3], is_training)
+    x = tf.nn.relu(x)
+    if is_training:
+        x = tf.nn.dropout(x, keep_prob)
+    # fully connected layer: fc2 -> batch norm -> ReLu -> output
+    x = fc_layer(x, variable_scopes[4])
+    if batch_norm:
+        x = batch_norm_layer(x, variable_scopes[4], is_training)
+    y = tf.nn.relu(x)
+    return y
+
+def train_cnn(graph, model, tf_data, cnn_shapes, hyperparams, epoches, *args):
+    print "Prepare network parameters", "."*32
+    with graph.as_default():
+        # Setup training, validation, testing dataset
+        with tf.name_scope("input"):
+            tf_train_dataset, tf_train_labels = tf_data['train_X'], tf_data['train_y']
+            tf_valid_dataset, tf_valid_labels = tf_data['valid_X'], tf_data['valid_y']
+            tf_test_dataset , tf_test_labels  = tf_data['test_X'] , tf_data['test_y']
+        # Initialize Weights and Biases
+        scopes = zip(*convnet_shapes)[0]
+        batch_norm = hyperparams['batch_norm']
+        initialize_variables(cnn_shapes, initializer=hyperparams['initializer'], batch_norm=batch_norm)
+          
+        # Unwrap HyperParameters
+        l2_reg = hyperparams['l2_reg'] # regularization penality factor
+        keep_prob, tfoptimizer = hyperparams['keep_prob'], hyperparams['optimizer']
+        init_lr,  global_step = hyperparams['init_lr'], tf.Variable(0)
+        decay_steps, decay_rate = hyperparams['decay_steps'], hyperparams['decay_rate']
+        with tf.name_scope('learning_rate'):
+            learning_rate = tf.train.exponential_decay(init_lr, global_step, decay_steps, decay_rate, staircase=True)
+        tf.scalar_summary('learning_rate', learning_rate)
+          
+        # Compute Loss Function and Predictions
+        train_logits = model(tf_train_dataset, scopes, True, batch_norm, keep_prob)
+        # Without regularization
+        with tf.name_scope('training_loss'):
+            train_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(train_logits, tf_train_labels))
+        tf.scalar_summary('training_loss', train_loss)
+        # With L2 regularization applied to fully connected layers
+        #l2_reg_loss = 0
+        #with tf.variable_scope('fc1', reuse=True):
+            #l2_reg_loss += tf.nn.l2_loss(tf.get_variable('wt')) + tf.nn.l2_loss(tf.get_variable('bi'))
+        #with tf.variable_scope('fc2', reuse=True):
+            #l2_reg_loss += tf.nn.l2_loss(tf.get_variable('wt')) + tf.nn.l2_loss(tf.get_variable('bi'))
+        #train_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(train_logits, tf_train_labels) + l2_reg*l2_reg_loss)
+        train_prediction = tf.nn.softmax(train_logits)
+        with tf.name_scope('training_accuracy'):
+            train_accuracy = compute_accuracy(train_prediction, tf_train_labels)
+        tf.scalar_summary('training_accuracy', train_accuracy)
+        # Optimizer
+        optimizer = tfoptimizer(learning_rate).minimize(train_loss, global_step=global_step)
+        
+        # Validation Computation
+        valid_logits = model(tf_valid_dataset, scopes, False, batch_norm, keep_prob)
+        with tf.name_scope('validation_loss'):
+            valid_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(valid_logits,tf_valid_labels))
+        tf.scalar_summary('validation_loss', valid_loss)
+        valid_prediction = tf.nn.softmax(valid_logits)
+        with tf.name_scope('validation_accuracy'):
+            valid_accuracy = compute_accuracy(valid_prediction, tf_valid_labels)
+        tf.scalar_summary('validation_accuracy', valid_accuracy)
+        # Test Computation
+        test_prediction = tf.nn.softmax(model(tf_test_dataset, scopes, False, batch_norm, keep_prob))
+        test_accuracy = compute_accuracy(test_prediction, tf_test_labels)
+
+        merge_summary_op = tf.merge_all_summaries()
+        
+  # Train Convnet
+    train_losses, valid_losses = np.zeros(epoches), np.zeros(epoches)
+    train_acc, valid_acc = np.zeros(epoches), np.zeros(epoches)
+    
+    
+    print "Start training", '.'*32
+    with tf.Session(graph=graph) as session:
+        train_writer = tf.train.SummaryWriter('logs/train', graph=session.graph)
+        valid_writer = tf.train.SummaryWriter('logs/valid', graph=session.graph)
+        tf.initialize_all_variables().run()
+        print('Initialized')
+        for epoch in range(epoches):
+            print "training epoch ", epoch
+            t = time.time()
+            offset = (epoch * batch_size) % (train_labels.shape[0] - batch_size)
+            batch_data = train_dataset[offset:(offset+batch_size), :, :, :]
+            batch_labels = train_labels[offset:(offset+batch_size), :]
+            feed_dict = {tf_train_dataset:batch_data, tf_train_labels:batch_labels}
+            # Run session...
+            lr, _, tl, tacc, train_summaries = session.run([learning_rate, optimizer, train_loss, train_accuracy, merge_summary_op], \
+                                                        feed_dict=feed_dict)
+            train_writer.add_summary(train_summaries, epoch)
+            
+            train_losses[epoch] = tl
+            train_acc[epoch] = tacc
+            # Compute validation set accuracy
+            vl, vacc, valid_summaries = session.run([valid_loss, valid_accuracy, merge_summary_op])
+            valid_writer.add_summary(valid_summaries, epoch)
+            valid_losses[epoch] = vl
+            valid_acc[epoch] = vacc
+            print('Epoch: %d:\tLoss: %f\t\tTime cost: %1.f\t\tTrain Acc: %.2f%%\tValid Acc: %2.f%%\tLearning rate: %.6f' \
+                %(epoch, tl, (time.time()-t), (tacc*100), (vacc*100), lr))
+        print "Finished training", '.'*32
+        # Compute test set accuracy
+        test_acc = session.run([test_accuracy])
+        print("Test accuracy: %2.f%%" %(test_acc*100))
+        train_writer.close()
+        valid_writer.close()
+        # Group training data into a dictionary
+        training_data = {'train_losses' : train_losses, 'train_acc' : train_acc, \
+        'valid_losses' : valid_losses, 'valid_acc' : valid_acc, 'test_acc' : test_acc}
+        train_writer.close()
+            
+    return graph, training_data
+
+if __name__=='__main__':
+    # Load Data
+    print "Load data", "."*32
+    train_dataset, train_labels, test_dataset, test_labels = load_data()
+      
+    # Split 20% of training set as validation set
+    print "Split training and validation set", "."*32
+    train_dataset, valid_dataset, train_labels, valid_labels = \
+    train_test_split(train_dataset, train_labels, test_size=10000,\
+                    random_state=897, stratify=train_labels)
+    # Print out data shapes
+    print 'Dataset\t\tFeatureShape\tLabelShape'
+    print 'Training set:\t', train_dataset.shape,'\t', train_labels.shape
+    print 'Validation set:\t', valid_dataset.shape,'\t', valid_labels.shape
+    print 'Testing set:\t', test_dataset.shape, '\t', test_labels.shape
+      
+    # Reshape the data into pixel by pixel by RGB channels
+    print "Reformat data", "."*32
+    train_dataset = np.rollaxis(train_dataset.reshape((-1,3,32,32)), 1, 4)
+    valid_dataset = np.rollaxis(valid_dataset.reshape((-1,3,32,32)), 1, 4)
+    test_dataset = np.rollaxis(test_dataset.reshape((-1,3,32,32)), 1, 4)
+    print 'Dataset\t\tFeatureShape\t\tLabelShape'
+    print 'Training set:\t', train_dataset.shape,'\t', train_labels.shape
+    print 'Validation set:\t', valid_dataset.shape, '\t', valid_labels.shape
+    print 'Testing set:\t', test_dataset.shape, '\t', test_labels.shape
+      
+    # Augment Data
+    print "Augment data", '.'*32
+    train_dataset, train_labels = augment_data(train_dataset, train_labels)
+    print 'Dataset\t\tFeatureShape\t\tLabelShape'
+    print 'Training set:\t', train_dataset.shape,'\t', train_labels.shape
+    print 'Validation set:\t', valid_dataset.shape, '\t', valid_labels.shape
+    print 'Testing set:\t', test_dataset.shape, '\t', test_labels.shape
+      
+    # Dataset Parameters
+    image_size = 32
+    num_labels = 10
+    num_channels = 3
+      
+    # Data Preprocess: change datatype; center the data
+    print "Preprocess data", "."*32
+    train_dataset, train_labels = preprocess_data(train_dataset, train_labels, num_labels)
+    valid_dataset, valid_labels = preprocess_data(valid_dataset, valid_labels, num_labels)
+    test_dataset,  test_labels  = preprocess_data(test_dataset,  test_labels,  num_labels)
+    print 'Dataset\t\tFeatureShape\t\tLabelShape'
+    print 'Training set:\t', train_dataset.shape,'\t', train_labels.shape
+    print 'Validation set:\t', valid_dataset.shape, '\t', valid_labels.shape
+    print 'Testing set:\t', test_dataset.shape, '\t', test_labels.shape
+      
+    # Network parameters
+    batch_size = 64
+    kernel_size3 = 3
+    kernel_size5 = 5
+    num_filter = 8
+    fc_size1 = 64
+      
+    # Setup shapes for each layer in the convnet
+    convnet_shapes = [['conv1', [kernel_size5, kernel_size5, num_channels, num_filter]],
+                      ['conv2', [kernel_size3, kernel_size3, num_filter, num_filter]]  ,
+                      ['conv3', [kernel_size5, kernel_size5, num_filter, num_filter]]  ,
+                      ['fc1'  , [(image_size/2/2/2)**2*num_filter, fc_size1]]        ,
+                      ['fc2'  , [fc_size1, num_labels]]]
+      
+    # Prepare data for tensorflow
+    graph = tf.Graph()
+    with graph.as_default():
+        tf_data = {'train_X': tf.placeholder(tf.float32, shape=(batch_size, image_size, image_size, num_channels), name='x-train'),
+                   'train_y': tf.placeholder(tf.float32, shape=(batch_size, num_labels), name='y-train'),
+                   'valid_X': tf.constant(valid_dataset, name='x-valid'), 'valid_y': tf.constant(valid_labels, name='y-valid'),
+                   'test_X' : tf.constant(test_dataset, name='x-test'),  'test_y' : tf.constant(test_labels, name='y-test')}
+        tfoptimizer = tf.train.AdamOptimizer
+      
+    # HyperParameters
+    hyperparams = {'keep_prob': 0.47, 'init_lr': 0.002, 'decay_rate': .9, 'decay_steps': 100,
+                   'optimizer': tfoptimizer, 'l2_reg': 0.096, 'batch_norm': True,
+                    'initializer': tf.truncated_normal_initializer(stddev=.015)} #tf.contrib.layers.variance_scaling_initializer()}
+      
+    # Setup computation graph and train convnet
+    steps = 31
+    model, save_data_name = cnn_model, 'training_data_stack3.1'
+    #model, save_data_name = convnet_inception, 'training_data_inception'
+    _, training_data = train_cnn(graph, model, tf_data, convnet_shapes, hyperparams, \
+                                    steps, True, train_dataset, train_labels, batch_size)
+      
+    # Save data
+    #with open(save_data_name, 'w') as fh:
+        #pickle.dump(training_data, fh)
+
+        
+
+                          
+                          
+
+
+
+
+
+                                     
