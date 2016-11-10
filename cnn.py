926b3429496e8dcc58b49274e886e17acf93f64d
Zhongyu Kuang <dream.kuang@gmail.com>
2016-11-09 22:36:25 -0500

    add fully connected layer

--- cnn.py
+++ cnn.py
@@ -1,13 +1,12 @@
 import abc
 import tensorflow as tf
 
-
 class layer(object):
 	'''an abstract metaclass for CNN layers'''
 	__metaclass__ = abc.ABCMeta
 
 	# static class variable
-	LAYER_TYPES = ['conv', 'fc', 'activation', 'batch_norm', 'dropout', 'pool']
+	LAYER_TYPES = ['conv2d', 'fc', 'activation', 'batch_norm', 'dropout', 'pool']
 	
 	@abc.abstractmethod
 	def get_layer_name(self):
@@ -29,7 +28,6 @@ class layer(object):
 		'''return y - resulted tensor after computation/training'''
 		pass
 
-
 	@classmethod
 	def variable_summaries(self, var, name):
 	    with tf.name_scope('summaries'):
@@ -43,18 +41,19 @@ class layer(object):
 	        tf.histogram_summary(name, var)
 
 
-class conv(layer):
+class conv2d(layer):
 
 	TRAINABLE = True
 
 	def __init__(self, layer_name, shape=None):
-		#layer.__init__(self)
-		#self._graph = graph
 		self._layer_name = layer_name
+		if shape:
+			if len(shape)!=4:
+				error_msg = "the shape for convolutional layer weights needs to be 4D, get " + str(len(shape)) + "D instead"
+				raise ValueError(error_msg)
 		self._shape = shape
 		self._wt_initializer = tf.truncated_normal_initializer(stddev=.01)
 		self._bi_initializer = tf.constant_initializer(1.0)
-
 
 	def get_layer_name(self):
 		return self._layer_name
@@ -77,7 +76,11 @@ class conv(layer):
 		"""
 		# Make sure the shape of the conv layer is define
 		if shape:
-			self._shape = shape
+			if len(shape)!=4:
+				error_msg = "the shape for convolutional layer weights needs to be 4D, get " + str(len(shape)) + "D instead"
+				raise ValueError(error_msg)
+			else:
+				self._shape = shape
 		else:
 			if not self._shape:
 				raise ValueError("shape is undefined! Must define shape to initalize conv layer!")
@@ -114,6 +117,137 @@ class conv(layer):
 				output = tf.nn.conv2d(input, wt, [1, stride, stride, 1], padding=padding) + bi
 				if add_output_summary:
 					tf.histogram_summary(self._layer_name+'/postConv', output)
+				return output
+
+
+class fc(layer):
+
+	TRAINABLE = True
+
+	def __init__(self, layer_name, shape=None):
+		self._layer_name = layer_name
+		if shape:
+			if len(shape)!=2:
+				error_msg = "the shape for convolutional layer weights needs to be 2D, get " + str(len(shape)) + "D instead"
+				raise ValueError(error_msg)
+		self._shape = shape
+		self._wt_initializer = tf.truncated_normal_initializer(stddev=.01)
+		self._bi_initializer = tf.constant_initializer(1.0)
+
+	def get_layer_name(self):
+		return self._layer_name
+
+	def get_layer_type(self):
+		return fc.__name__
+
+	def is_trainable(self):
+		return fc.TRAINABLE
+
+	def get_shape(self):
+		if self._shape:
+			return self._shape
+		else:
+			return "shape is undefined!"
+
+	def initialize(self, shape=None, wt_initializer=None, bi_initializer=None):
+		"""initialize weights and biases based on given initializers.
+		If initializers are not given, using default initializers created in the constructor
+		"""
+		# Make sure the shape of the conv layer is define
+		if shape:
+			if len(shape)!=2:
+				error_msg = "the shape for convolutional layer weights needs to be 2D, get " + str(len(shape)) + "D instead"
+				raise ValueError(error_msg)
+			else:
+				self._shape = shape
+		else:
+			if not self._shape:
+				raise ValueError("shape is undefined! Must define shape to initalize conv layer!")
+
+		# Use the initializers passed along by the users instead of the default initializers if they are given
+		if wt_initializer:
+			self._wt_initializer = wt_initializer
+
+		if bi_initializer:
+			self._bi_initializer = bi_initializer
+
+		with tf.variable_scope(self._layer_name, reuse=None) as scope:
+			wt = tf.get_variable('weights', self._shape, initializer=self._wt_initializer)
+			bi = tf.get_variable('biases', self._shape[-1], initializer=self._bi_initializer)
+			scope.reuse_variables()
+
+	def get_variables(self):
+		with tf.variable_scope(self._layer_name, reuse=True):
+			wt = tf.get_variable('weights')
+			bi = tf.get_variable('biases')
+		return wt, bi
+
+	def add_variable_summaries(self):
+		with tf.variable_scope(self._layer_name, reuse=True):
+			self.variable_summaries(tf.get_variable("weights"), self._layer_name+'/weights')
+			self.variable_summaries(tf.get_variable("biases"), self._layer_name+'/biases')
+
+	def train(self, input, add_output_summary=True):
+		'''input should be a tensor'''
+		with tf.variable_scope(self._layer_name, reuse=True):
+			wt = tf.get_variable('weights')
+			bi = tf.get_variable('biases')
+			with tf.name_scope('postFC'):
+				output = tf.matmul(input, wt) + bi
+				if add_output_summary:
+					tf.histogram_summary(self._layer_name+'/postFC', output)
+				return output
+
+
+class activation(layer):
+
+	TRAINABLE = False
+
+	def __init__(self, layer_name, act_func=None):
+		self._layer_name = layer_name
+		self.
+
+	def get_layer_name(self):
+		return self._layer_name
+
+	def get_layer_type(self):
+		return fc.__name__
+
+	def is_trainable(self):
+		return fc.TRAINABLE
+
+		# Use the initializers passed along by the users instead of the default initializers if they are given
+		if wt_initializer:
+			self._wt_initializer = wt_initializer
+
+		if bi_initializer:
+			self._bi_initializer = bi_initializer
+
+		with tf.variable_scope(self._layer_name, reuse=None) as scope:
+			wt = tf.get_variable('weights', self._shape, initializer=self._wt_initializer)
+			bi = tf.get_variable('biases', self._shape[-1], initializer=self._bi_initializer)
+			scope.reuse_variables()
+
+	def get_variables(self):
+		with tf.variable_scope(self._layer_name, reuse=True):
+			wt = tf.get_variable('weights')
+			bi = tf.get_variable('biases')
+		return wt, bi
+
+	def add_variable_summaries(self):
+		with tf.variable_scope(self._layer_name, reuse=True):
+			self.variable_summaries(tf.get_variable("weights"), self._layer_name+'/weights')
+			self.variable_summaries(tf.get_variable("biases"), self._layer_name+'/biases')
+
+	def train(self, input, add_output_summary=True):
+		'''input should be a tensor'''
+		with tf.variable_scope(self._layer_name, reuse=True):
+			wt = tf.get_variable('weights')
+			bi = tf.get_variable('biases')
+			with tf.name_scope('postFC'):
+				output = tf.matmul(input, wt) + bi
+				if add_output_summary:
+					tf.histogram_summary(self._layer_name+'/postFC', output)
 				return output
 
 if __name__=='__main__':
--- test_cnn.py
+++ test_cnn.py
@@ -3,9 +3,14 @@ import numpy as np
 from cnn import *
 
 def test_conv(steps):
+	try:
+		convx = conv('convx', [1,2])
+	except ValueError:
+		print "Successfully catch shape error"
+
 	sess = tf.InteractiveSession()
 	shape = [1, 1, 3, 1]
-	conv1 = conv('conv1', shape)
+	conv1 = conv('conv1', shape) 
 	assert(conv1.get_layer_name()=='conv1')
 	assert(conv1.get_layer_type()=='conv')
 	assert(conv1.is_trainable()==True)
@@ -14,21 +19,53 @@ def test_conv(steps):
 	conv1.add_variable_summaries()
 	wt, bi = conv1.get_variables()
 	sess.run([tf.initialize_all_variables()])
-	wt, bi = sess.run([wt, bi])
-	print "initialized weights:", wt
-	print "initialized biases:", bi
-	print '*'*32
+	wt_val, bi_val = sess.run([wt, bi])
+	print "initialized weights:", wt_val
+	print "initialized biases:", bi_val
 
 	input = tf.placeholder(tf.float32, [4, 2, 2, 3])
-
 	for i in range(steps):
 		X_np = np.random.randn(4, 2, 2, 3)
 		print '*'*16, i, '*'*16
 		y = conv1.train(input)
 		print y.eval(feed_dict={input:X_np})
-	
+	sess.close()
+
+def test_fc(steps):
+	try:
+		fcx = fc('fcx', [1,2,3,4])
+	except ValueError:
+		print "Successfully catch shape error"
+
+	sess = tf.InteractiveSession()
+	shape = [2, 1]
+	fc1 = fc('fc1', shape)
+	assert(fc1.get_layer_name()=='fc1')
+	assert(fc1.get_layer_type()=='fc')
+	assert(fc1.is_trainable()==True)
+	assert(fc1.get_shape()==shape)
+	fc1.initialize()
+	fc1.add_variable_summaries()
+	wt, bi = fc1.get_variables()
+	sess.run(tf.initialize_all_variables())
+	wt_val, bi_val = sess.run([wt, bi])
+	print "initialized weights: ", wt_val
+	print "initialized biases: ", bi_val
+
+	input = tf.placeholder(tf.float32, [1, 2])
+	for i in range(steps):
+		X_np = np.random.randn(1, 2)
+		print '*'*16, i, '*'*16
+		y = fc1.train(input)
+		print y.eval(feed_dict={input:X_np})
 	sess.close()
 
 if __name__=='__main__':
-	test_conv(3)
+	test_conv_bool = raw_input("Test conv layer? [y] or [n]")
+	if test_conv_bool=='y':
+		test_conv(1)
+
+	test_fc_bool = raw_input("Test fc layer? [y] or [n]")
+	if test_fc_bool=='y':
+		test_fc(2)
 
