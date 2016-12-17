66aac8f8df56dfcd354663425b2e01d1f32898b8
Zhongyu Kuang <dream.kuang@gmail.com>
2016-12-17 12:15:45 -0800

    update ignore list and modify prepare_input.py for zca whitening

--- cifar10/prepare_input.py
+++ cifar10/prepare_input.py
@@ -8,7 +8,7 @@ from preprocess import *
 def unpickle(file):
     # Load pickled data
     fo = open(file, 'rb')
-    dict = pickle.load(fo)
+    dict = pickle.load(fo) 
     fo.close()
     return dict
 
@@ -32,7 +32,7 @@ def load_data(data_dir):
     test_filename = data_dir+'test_batch'
     train_dataset = None
     test_dataset = None
-    print "Loading the training data..."
+    print("Loading the training data...")
     for i in range(1,6):
         train_filename = train_fnroot + str(i)
         batch = unpickle(train_filename)
@@ -42,7 +42,7 @@ def load_data(data_dir):
         else:
             train_dataset = np.concatenate((train_dataset, batch['data']), axis=0)
             train_labels = np.concatenate((train_labels, batch['labels']))
-    print "Loading the testing data..."
+    print("Loading the testing data...")
     test_batch = unpickle(test_filename)
     test_dataset = test_batch['data']
     test_labels = np.array(test_batch['labels'])
@@ -61,44 +61,48 @@ def augment_data(features, labels):
     return cat_features, cat_labels
 
 def preprocess_data(X, y, num_labels):
-    # 1) Center the training data/Subtract Mean
-    # 2) One-hot encode labels
-    # 3) Random Permute samples
-    # 4) Change datatype to np.float32 to speed up
+    # 1) One-hot encode labels
+    # 2) Random Permute samples
+    # 3) Change datatype to np.float32 to speed up
     # zero-mean and zca-whitening
-    X_centered = center_data(X)
-    X_whitened = zca_whiten(X_centered)
     y_encoded = np.arange(num_labels)==y[:, None]
     perm = np.random.permutation(y_encoded.shape[0])
-    X_preprocessed = X_whitened[perm]
+    X_preprocessed = X[perm]
     y_encoded = y_encoded[perm]
-    return X_centered.astype(np.float32), y_encoded.astype(np.float32)
+    return X_preprocessed.astype(np.float32), y_encoded.astype(np.float32)
+
+def whiten_data(X):
+    X_centered = center_data(X)
+    X_whitened = zca_whiten(X_centered)
+    return X_whitened
 
 def prepare_cifar10_input(data_dir, augmentation=False):
     # Load Data
-    print "Load data", "."*32
+    print("Load data", "."*32)
     train_dataset, train_labels, test_dataset, test_labels = load_data(data_dir)
+    train_dataset = whiten_data(train_dataset)
+    test_dataset = whiten_data(test_dataset)
 
     # Split 20% of training set as validation set
-    print "Split training and validation set", "."*32    
+    print("Split training and validation set", "."*32)
     train_dataset, valid_dataset, train_labels, valid_labels = \
     train_test_split(train_dataset, train_labels, test_size=5000,\
     random_state=897, stratify=train_labels)
     # Print out data shapes
-    print 'Dataset\t\tFeatureShape\tLabelShape'
-    print 'Training set:\t', train_dataset.shape,'\t', train_labels.shape
-    print 'Validation set:\t', valid_dataset.shape,'\t', valid_labels.shape
-    print 'Testing set:\t', test_dataset.shape, '\t', test_labels.shape
+    print('Dataset\t\tFeatureShape\tLabelShape')
+    print('Training set:\t', train_dataset.shape,'\t', train_labels.shape)
+    print('Validation set:\t', valid_dataset.shape,'\t', valid_labels.shape)
+    print('Testing set:\t', test_dataset.shape, '\t', test_labels.shape)
 
     # Reshape the data into pixel by pixel by RGB channels
-    print "Reformat data", "."*32
+    print("Reformat data", "."*32)
     train_dataset = np.rollaxis(train_dataset.reshape((-1,3,32,32)), 1, 4)
     valid_dataset = np.rollaxis(valid_dataset.reshape((-1,3,32,32)), 1, 4)
     test_dataset = np.rollaxis(test_dataset.reshape((-1,3,32,32)), 1, 4)
-    print 'Dataset\t\tFeatureShape\t\tLabelShape'
-    print 'Training set:\t', train_dataset.shape,'\t', train_labels.shape
-    print 'Validation set:\t', valid_dataset.shape, '\t', valid_labels.shape
-    print 'Testing set:\t', test_dataset.shape, '\t', test_labels.shape
+    print('Dataset\t\tFeatureShape\t\tLabelShape')
+    print('Training set:\t', train_dataset.shape,'\t', train_labels.shape)
+    print('Validation set:\t', valid_dataset.shape, '\t', valid_labels.shape)
+    print('Testing set:\t', test_dataset.shape, '\t', test_labels.shape)
 
     # Dataset Parameters
     image_size = 32
@@ -110,15 +114,15 @@ def prepare_cifar10_input(data_dir, augmentation=False):
         train_dataset, train_labels = augment_data(train_dataset, train_labels)
 
     # Data Preprocess: change datatype; center the data
-    print "Preprocess data", "."*32
+    print("Preprocess data", "."*32)
     train_dataset, train_labels = preprocess_data(train_dataset, train_labels, num_labels)
     valid_dataset, valid_labels = preprocess_data(valid_dataset, valid_labels, num_labels)
     test_dataset,  test_labels  = preprocess_data(test_dataset,  test_labels,  num_labels)
     dataset_list = [train_dataset, train_labels, valid_dataset, valid_labels, test_dataset, test_labels]
-    print 'Dataset\t\tFeatureShape\t\tLabelShape'
-    print 'Training set:\t', train_dataset.shape,'\t', train_labels.shape
-    print 'Validation set:\t', valid_dataset.shape, '\t', valid_labels.shape
-    print 'Testing set:\t', test_dataset.shape, '\t', test_labels.shape
+    print('Dataset\t\tFeatureShape\t\tLabelShape')
+    print('Training set:\t', train_dataset.shape,'\t', train_labels.shape)
+    print('Validation set:\t', valid_dataset.shape, '\t', valid_labels.shape)
+    print('Testing set:\t', test_dataset.shape, '\t', test_labels.shape)
     return dataset_list
 
 def plot_data(data):
