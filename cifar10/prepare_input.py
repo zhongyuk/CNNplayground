bacfa98d170d37aaa523cbd2fbfc5c58ef2216f4
Zhongyu Kuang <dream.kuang@gmail.com>
2016-12-19 14:48:47 -0800

    add data augmentation methods and update cifar10 preprocess procedures

--- cifar10/prepare_input.py
+++ cifar10/prepare_input.py
@@ -48,7 +48,7 @@ def load_data(data_dir):
     test_labels = np.array(test_batch['labels'])
     return train_dataset, train_labels,  test_dataset, test_labels
 
-def augment_data(features, labels):
+def augment_data_cifar10(features, labels):
     # 50% Upside Down Filp; 50% Mirror Flip
     ud_ind = np.random.binomial(1, .5, features.shape[0]).astype(np.bool)
     lf_ind = np.invert(ud_ind)
@@ -60,7 +60,7 @@ def augment_data(features, labels):
     cat_labels = np.concatenate((labels, ud_labels, lf_labels))
     return cat_features, cat_labels
 
-def preprocess_data(X, y, num_labels):
+def preprocess_data_cifar10(X, y, num_labels):
     # 1) One-hot encode labels
     # 2) Random Permute samples
     # 3) Change datatype to np.float32 to speed up
@@ -75,6 +75,63 @@ def whiten_data(X):
     X_centered = center_data(X)
     X_whitened = zca_whiten(X_centered)
     return X_whitened
+
+def one_hot_encode(y, num_labels):
+    '''One-Hot Encode labels'''
+    y_encoded = np.arange(num_labels)==y[:, None]
+    return y_encoded
+
+def prepare_cifar10_input_new(data_dir, augmentation=True):
+    print("Load data", "*"*32)
+    train_dataset, train_labels, test_dataset, test_labels = load_data(data_dir)
+
+    # Split 20% of training set as validation set
+    print("Split training and validation set", "."*32)
+    train_dataset, valid_dataset, train_labels, valid_labels = \
+    train_test_split(train_dataset, train_labels, test_size=5000,\
+    random_state=897, stratify=train_labels)
+
+    # Reshape the data into pixel by pixel by RGB channels
+    print("Reformat data", "."*32)
+    train_dataset = np.rollaxis(train_dataset.reshape((-1,3,32,32)), 1, 4)
+    valid_dataset = np.rollaxis(valid_dataset.reshape((-1,3,32,32)), 1, 4)
+    test_dataset = np.rollaxis(test_dataset.reshape((-1,3,32,32)), 1, 4)
+    print('Dataset\t\tFeatureShape\t\tLabelShape')
+    print('Training set:\t', train_dataset.shape,'\t', train_labels.shape)
+    print('Validation set:\t', valid_dataset.shape, '\t', valid_labels.shape)
+    print('Testing set:\t', test_dataset.shape, '\t', test_labels.shape)
+
+    if augmentation:
+        print('Augmenting training data......')
+        train_dataset, train_labels = augment_data(train_dataset, train_labels)
+    
+    print("Whitening data......")
+    train_X = whiten_data(train_dataset.reshape((train_labels.shape[0],-1)))
+    valid_X = whiten_data(valid_dataset.reshape((valid_labels.shape[0],-1)))
+    test_X  = whiten_data(test_dataset.reshape((test_labels.shape[0],-1)))
+    train_dataset = train_X.reshape((-1,3,32,32))
+    valid_dataset = valid_X.reshape((-1,3,32,32))
+    test_dataset = test_X.reshape((-1,3,32,32))
+    print("One-Hot Encoding labels......")
+    num_labels = 10
+    train_labels = one_hot_encode(train_labels, num_labels)
+    valid_labels = one_hot_encode(valid_labels, num_labels)
+    test_labels = one_hot_encode(test_labels, num_labels)
+
+    train_dataset = train_dataset.astype(np.float32)
+    valid_dataset = valid_dataset.astype(np.float32)
+    test_dataset = test_dataset.astype(np.float32)
+    train_labels = train_labels.astype(np.float32)
+    valid_labels = valid_labels.astype(np.float32)
+    test_labels = test_labels.astype(np.float32)
+    dataset_list = [train_dataset, train_labels, valid_dataset, valid_labels, test_dataset, test_labels]
+    print('Dataset\t\tFeatureShape\t\tLabelShape')
+    print('Training set:\t', train_dataset.shape,'\t', train_labels.shape)
+    print('Validation set:\t', valid_dataset.shape, '\t', valid_labels.shape)
+    print('Testing set:\t', test_dataset.shape, '\t', test_labels.shape)
+    return dataset_list
+
+
 
 def prepare_cifar10_input(data_dir, augmentation=False):
     # Load Data
@@ -111,13 +168,13 @@ def prepare_cifar10_input(data_dir, augmentation=False):
 
     # Data Augmentation
     if augmentation:
-        train_dataset, train_labels = augment_data(train_dataset, train_labels)
+        train_dataset, train_labels = augment_data_cifar10(train_dataset, train_labels)
 
     # Data Preprocess: change datatype; center the data
     print("Preprocess data", "."*32)
-    train_dataset, train_labels = preprocess_data(train_dataset, train_labels, num_labels)
-    valid_dataset, valid_labels = preprocess_data(valid_dataset, valid_labels, num_labels)
-    test_dataset,  test_labels  = preprocess_data(test_dataset,  test_labels,  num_labels)
+    train_dataset, train_labels = preprocess_data_cifar10(train_dataset, train_labels, num_labels)
+    valid_dataset, valid_labels = preprocess_data_cifar10(valid_dataset, valid_labels, num_labels)
+    test_dataset,  test_labels  = preprocess_data_cifar10(test_dataset,  test_labels,  num_labels)
     dataset_list = [train_dataset, train_labels, valid_dataset, valid_labels, test_dataset, test_labels]
     print('Dataset\t\tFeatureShape\t\tLabelShape')
     print('Training set:\t', train_dataset.shape,'\t', train_labels.shape)
@@ -147,4 +204,5 @@ def train_data_summary(filename):
 
 if __name__=='__main__':
     data_dir = "./data/"
-    prepare_cifar10_input(data_dir)
+    #prepare_cifar10_input(data_dir)
+    prepare_cifar10_input_new(data_dir)
